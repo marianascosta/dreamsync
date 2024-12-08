3962db45e5822eed0c574ba87e82b8ab79a169e7
Mariana Costa <marianaamaralscosta@gmail.com>
2024-12-08 11:09:18 +0000

    Add dreams and profile storage service.

---
+++ app/src/main/java/com/example/dreamsync/data/models/Dream.kt
@@ -0,0 +1,10 @@
+package com.example.dreamsync.data.models
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Dream(
+    val title: String,
+    val description: String,
+    val date: String
+)
