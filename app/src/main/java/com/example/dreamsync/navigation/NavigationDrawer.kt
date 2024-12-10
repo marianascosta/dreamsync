939eac45cedf40657a91b14dd18653e23338c2e4
borgez31 <fc58187@alunos.fc.ul.pt>
2024-12-10 15:54:03 +0000

    Dependencies changed and Edited Drawer

--- app/src/main/java/com/example/dreamsync/navigation/NavigationDrawer.kt
+++ app/src/main/java/com/example/dreamsync/navigation/NavigationDrawer.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 
@@ -15,7 +16,11 @@ fun NavigationDrawer(
     onCloseDrawer: () -> Unit
 ) {
     ModalDrawerSheet {
-        Text("Drawer Title", modifier = Modifier.padding(16.dp))
+        Text(
+            "Explore",
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(16.dp)
+        )
         NavigationDrawerItem(
             label = { Text("Home") },
             selected = selectedIndex == 1,
