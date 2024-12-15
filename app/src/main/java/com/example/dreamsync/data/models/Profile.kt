9abb5c7c714f9f7c634b84c02175a319701f2a50
borgez31 <fc58187@alunos.fc.ul.pt>
2024-12-15 13:25:02 +0000

    Profile redo

--- app/src/main/java/com/example/dreamsync/data/models/Profile.kt
+++ app/src/main/java/com/example/dreamsync/data/models/Profile.kt
@@ -3,6 +3,12 @@ package com.example.dreamsync.data.models
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class Profile(val name: String = "") {
-    constructor() : this("") // This is the no-argument constructor Firebase requires
+data class Profile(
+    val userName: String = "",
+    val userEmail: String = "",
+    val userBio: String = "",
+    val preferredRole: String = "",
+    val profilePicture: String = ""
+) {
+    constructor() : this("", "", "", "", "")
 }
