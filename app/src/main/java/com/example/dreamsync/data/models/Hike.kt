4d5d4862256e2964096eefd345200038497b441a
borgez31 <fc58187@alunos.fc.ul.pt>
2024-12-18 12:17:02 +0000

    Layer add

--- app/src/main/java/com/example/dreamsync/data/models/Hike.kt
+++ app/src/main/java/com/example/dreamsync/data/models/Hike.kt
@@ -3,24 +3,28 @@ package com.example.dreamsync.data.models
 import kotlinx.serialization.Serializable
 import java.util.UUID
 
+
 @Serializable
 class Hike (
     val id: String,
     val name: String,
     val description: String,
-    val layers: Int,
+    val numLayers: Int,
+    val layers: List<Layer>,
     val isComplete: Boolean
 ) {
     constructor(
         name: String,
         description: String,
-        layers: Int,
+        numLayers: Int,
+        layers: List<Layer>,
         isComplete: Boolean
     ) : this(
         id = UUID.randomUUID().toString(),
         name = name,
         description = description,
-        layers = layers,
+        numLayers = numLayers,
+        layers = emptyList(),
         isComplete = isComplete
     )
 }
